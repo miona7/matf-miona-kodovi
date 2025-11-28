27dda8e082ec6e4be3744c9443f6b0a57e239e23
miona <sretenovicmiona7@gmail.com>
2025-11-28 20:39:02 +0100

    finished main.cpp

--- razvoj_softvera/cas02/points/main.cpp
+++ razvoj_softvera/cas02/points/main.cpp
@@ -14,4 +14,14 @@ struct Point {
 int main() {
     Point p1 {3.5, 4.3};  // lista inicijalizacije, clanovi strukture se direktno inicijalizuju vrednostima
     p1.print();
+
+    Point* p2 = new Point{1.2, 2.3};
+    p2->print(); 
+    delete p2;
+
+    Point* points = new Point[] { {1.2, 3.7}, {8.9, 9.5} };
+    for(int i = 0; i < 2; i++) {
+        points[i].print();
+    }
+    delete[] points; 
 }

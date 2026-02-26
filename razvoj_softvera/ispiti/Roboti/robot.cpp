f8be400802c52d2edf82962caff21852c0af59a4
miona <sretenovicmiona7@gmail.com>
2026-02-26 18:55:40 +0100

    feat(rs/ispiti/Roboti): method attack

--- razvoj_softvera/ispiti/Roboti/robot.cpp
+++ razvoj_softvera/ispiti/Roboti/robot.cpp
@@ -1,9 +1,13 @@
 #include "robot.h"
 
+Robot::Robot(const QVariant& variant) {
+    fromQVariant(variant);
+}
+
 void Robot::fromQVariant(const QVariant& variant) {
     auto map = variant.toMap();
     m_name = map.value("name").toString();
-    m_maxLifePoens = m_currLifePoens = map.value("hp").toUInt();
+    m_maxLifePoints = m_currLifePoints = map.value("hp").toUInt();
 
     auto atkList = map.value("atk").toList();
     m_minStrengthAttack = atkList[0].toUInt();
@@ -11,10 +15,32 @@ void Robot::fromQVariant(const QVariant& variant) {
 }
 
 QString Robot::toQString() const {
-    return m_name + " : hp=" + QString::number(m_maxLifePoens) + ", atk=[" +
+    return m_name + " : hp=" + QString::number(m_maxLifePoints) + ", atk=[" +
            QString::number(m_minStrengthAttack) + "," + QString::number(m_maxStrengthAttack) + "]";
 }
 
 bool Robot::attack(Robot& target) const {
-    return false;
+    unsigned damage = QRandomGenerator::global()->bounded(m_minStrengthAttack, m_maxStrengthAttack + 1);
+    double scaling = std::max(0.5, static_cast<double>(m_currLifePoints) / m_maxLifePoints);
+    unsigned scaledDamage = scaling * damage;
+
+    if(scaledDamage >= target.getCurrLifePoints()) {
+        target.setCurrLifePoints(0);
+    } else {
+        target.setCurrLifePoints(target.getCurrLifePoints() - scaledDamage);
+    }
+
+    return target.isDead();
+}
+
+bool Robot::isDead() const {
+    return m_currLifePoints == 0;
+}
+
+unsigned Robot::getCurrLifePoints() const {
+    return m_currLifePoints;
+}
+
+void Robot::setCurrLifePoints(unsigned points) {
+    m_currLifePoints = points;
 }
--- razvoj_softvera/ispiti/Roboti/robot.h
+++ razvoj_softvera/ispiti/Roboti/robot.h
@@ -8,17 +8,23 @@
 class Robot {
 public:
     Robot() = default;
+    Robot(const QVariant&);
 
     void fromQVariant(const QVariant&);
     QString toQString() const;
     bool attack(Robot&) const;
 
+    bool isDead() const;
+
+    unsigned getCurrLifePoints() const;
+    void setCurrLifePoints(unsigned);
+
 private:
     QString m_name;
     unsigned m_minStrengthAttack;
     unsigned m_maxStrengthAttack;
-    unsigned m_maxLifePoens;
-    unsigned m_currLifePoens;
+    unsigned m_maxLifePoints;
+    unsigned m_currLifePoints;
 };
 
 #endif // ROBOT_H

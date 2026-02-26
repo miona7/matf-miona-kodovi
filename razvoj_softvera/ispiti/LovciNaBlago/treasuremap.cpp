10217d64c7a14a093c31afb698d70453c94cf527
miona <sretenovicmiona7@gmail.com>
2026-02-26 23:20:47 +0100

    feat(rs/ispiti/LovciNaBlago): add methods to TreasureMap class
    - getMovableFrom
    - grabTreasure
    - hasDucats

--- razvoj_softvera/ispiti/LovciNaBlago/treasuremap.cpp
+++ razvoj_softvera/ispiti/LovciNaBlago/treasuremap.cpp
@@ -17,6 +17,7 @@ namespace {
 
 void TreasureMap::fromQVariant(const QVariant& variant) {
     m_tiles.clear();
+    m_numOfDucats = 0;
 
     const auto rowList = variant.toList();
     for(int i = 0; i < rowList.size(); i++) {
@@ -25,17 +26,54 @@ void TreasureMap::fromQVariant(const QVariant& variant) {
         for(const auto& numVar : row) {
             const auto tile = numToTile(numVar.toInt());
             m_tiles[i].push_back(tile);
+            if(tile == TreasureMap::Tile::Treasure) {
+                m_numOfDucats++;
+            }
         }
     }
 }
 
-QPair<int, int> TreasureMap::getMapSize() const {
+QPair<unsigned, unsigned> TreasureMap::getMapSize() const {
     if(m_tiles.isEmpty()) {
         return {0, 0};
     }
     return {m_tiles.size(), m_tiles[0].size()};
 }
 
-TreasureMap::Tile TreasureMap::getTileAt(const QPair<int, int>& pos) const {
+TreasureMap::Tile TreasureMap::getTileAt(const QPair<unsigned, unsigned>& pos) const {
     return m_tiles[pos.first][pos.second];
 }
+
+QList<QPair<unsigned, unsigned>> TreasureMap::getMovableFrom(const QPair<unsigned, unsigned>& pos) const {
+    QList<QPair<unsigned, unsigned>> positions;
+    const auto [row, column] = pos;
+
+    if(row > 0 && m_tiles[row - 1][column] != TreasureMap::Tile::Wall) {
+        positions.append({row - 1, column});
+    }
+    if(row < m_tiles.size() && m_tiles[row + 1][column] != TreasureMap::Tile::Wall) {
+        positions.append({row + 1, column});
+    }
+    if(column > 0 && m_tiles[row][column - 1] != TreasureMap::Tile::Wall) {
+        positions.append({row, column - 1});
+    }
+    if(column < m_tiles[row].size() && m_tiles[row][column + 1] != TreasureMap::Tile::Wall) {
+        positions.append({row, column + 1});
+    }
+
+    return positions;
+}
+
+bool TreasureMap::grabTreasure(const QPair<unsigned, unsigned>& pos) {
+    if(m_tiles[pos.first][pos.second] == TreasureMap::Tile::Treasure) {
+        m_tiles[pos.first][pos.second] = TreasureMap::Tile::Clear;
+        m_numOfDucats--;
+        return true;
+    }
+    return false;
+}
+
+bool TreasureMap::hasDucats() const {
+    return m_numOfDucats != 0;
+}
+
--- razvoj_softvera/ispiti/LovciNaBlago/treasuremap.h
+++ razvoj_softvera/ispiti/LovciNaBlago/treasuremap.h
@@ -14,11 +14,16 @@ public:
     TreasureMap() = default;
 
     void fromQVariant(const QVariant&);
-    QPair<int, int> getMapSize() const;
-    TreasureMap::Tile getTileAt(const QPair<int, int>&) const;
+    QPair<unsigned, unsigned> getMapSize() const;
+    TreasureMap::Tile getTileAt(const QPair<unsigned, unsigned>&) const;
+    QList<QPair<unsigned, unsigned>> getMovableFrom(const QPair<unsigned, unsigned>&) const;
+
+    bool grabTreasure(const QPair<unsigned, unsigned>&);
+    bool hasDucats() const;
 
 private:
     QList<QList<Tile>> m_tiles;
+    unsigned m_numOfDucats;
 };
 
 #endif // TREASUREMAP_H

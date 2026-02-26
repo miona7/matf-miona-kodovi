62c376a9a416d5960086cacc8e9f57b661a36713
miona <sretenovicmiona7@gmail.com>
2026-02-26 11:47:48 +0100

    feat(rs/ispiti/Roboti): add private members for class Robot and methods fromQVariant and toQString
    - TODO: method attack

--- razvoj_softvera/ispiti/Roboti/robot.cpp
+++ razvoj_softvera/ispiti/Roboti/robot.cpp
@@ -1,2 +1,20 @@
 #include "robot.h"
 
+void Robot::fromQVariant(const QVariant& variant) {
+    auto map = variant.toMap();
+    m_name = map.value("name").toString();
+    m_maxLifePoens = m_currLifePoens = map.value("hp").toUInt();
+
+    auto atkList = map.value("atk").toList();
+    m_minStrengthAttack = atkList[0].toUInt();
+    m_minStrengthAttack = atkList[1].toUInt();
+}
+
+QString Robot::toQString() const {
+    return m_name + " : hp=" + QString::number(m_maxLifePoens) + ", atk=[" +
+           QString::number(m_minStrengthAttack) + "," + QString::number(m_maxStrengthAttack) + "]";
+}
+
+bool Robot::attack(Robot& target) const {
+    return false;
+}
--- razvoj_softvera/ispiti/Roboti/robot.h
+++ razvoj_softvera/ispiti/Roboti/robot.h
@@ -1,9 +1,24 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#include <QString>
+#include <QVariant>
+#include <QRandomGenerator>
+
 class Robot {
 public:
     Robot() = default;
+
+    void fromQVariant(const QVariant&);
+    QString toQString() const;
+    bool attack(Robot&) const;
+
+private:
+    QString m_name;
+    unsigned m_minStrengthAttack;
+    unsigned m_maxStrengthAttack;
+    unsigned m_maxLifePoens;
+    unsigned m_currLifePoens;
 };
 
 #endif // ROBOT_H

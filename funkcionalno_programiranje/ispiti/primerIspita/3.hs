dd38ac4c090a4f074f01f85225836362020596e1
miona <sretenovicmiona7@gmail.com>
2026-06-07 16:23:57 +0200

    feat(fp/ispiti/primerIspita): add 3.hs

--- funkcionalno_programiranje/ispiti/primerIspita/3.hs
+++ funkcionalno_programiranje/ispiti/primerIspita/3.hs
@@ -1,5 +1,7 @@
 module Zad3 where
 
+import Test.QuickCheck
+
 -- Zadatak 3:
 
 -- Definisati strukturu podataka `Stek a`, gde je `a`
@@ -18,3 +20,26 @@ module Zad3 where
 -- Za prazan stek:  </>
 
 -- Instancirati Functor nad `Stek a`
+
+newtype Stek a = Stek [a] 
+-- data Stek' a = Empty | a (Stek' a)
+
+fromList :: [a] -> Stek a
+fromList = Stek
+
+toList :: Stek a -> [a]
+toList (Stek lst) = lst
+
+prop_fromList_toList :: (Eq a) => [a] -> Bool
+prop_fromList_toList lst = (toList $ fromList lst) == lst
+
+instance Show a => Show (Stek a) where
+    show s = "<" ++ show' s ++ ">"
+        where
+            show' :: Stek a -> String
+            show' (Stek []) = "/"
+            show' (Stek [x]) = show x
+            show' (Stek (x:xs)) = show x ++ ", " ++ show' (Stek xs)
+
+instance Functor Stek where
+    fmap f (Stek lst) = fromList $ fmap f lst 

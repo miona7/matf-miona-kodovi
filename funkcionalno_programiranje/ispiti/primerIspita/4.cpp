59655059b78cc9ca343b732e797ab6772a681ec0
miona <sretenovicmiona7@gmail.com>
2026-06-09 22:08:14 +0200

    feat(fp/ispiti/primerIspita): add 4.cpp and CMakeLists.txt

--- funkcionalno_programiranje/ispiti/primerIspita/4.cpp
+++ funkcionalno_programiranje/ispiti/primerIspita/4.cpp
@@ -1,7 +1,6 @@
 // Zadatak 4
 // Koristeći biblioteku range-v3, a bez upotrebe for, while, do-while petlji,
-// komande goto, rekurzije, i algortima std::for_each implementirati sledeći
-// zadatak:
+// komande goto, rekurzije, i algoritma std::for_each implementirati sledeći zadatak:
 // Napisati program koji sa standardnog ulaza čita reč po reč i
 // briše uzastopna ponavljanja iste reči. Svaku reč ispisuje u novom redu.
 //
@@ -13,8 +12,7 @@
 // thunder thunder
 // thunder thunder
 //
-// Treba ispisati
-//
+// Treba ispisati:
 // thunder
 // i
 // was
@@ -32,7 +30,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <functional>
+#include <iterator>
 
 #include <range/v3/view.hpp>
 #include <range/v3/action.hpp>
@@ -44,6 +42,16 @@
 using namespace ranges::v3;
 namespace views = ranges::v3::view;
 namespace actions = ranges::v3::action;
-
+ 
 int main(int argc, char** argv) {   
+    std::vector<std::string> allWords{
+        std::istream_iterator<std::string>{std::cin}, // pocetak strima
+        std::istream_iterator<std::string>{} // kraj strima
+    };
+
+    const auto words = allWords | views::unique; 
+
+    for(auto word : words) {
+        std::cout << word << std::endl;
+    }
 }
